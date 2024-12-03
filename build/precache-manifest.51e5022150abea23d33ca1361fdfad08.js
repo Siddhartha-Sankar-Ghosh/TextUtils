6d42a0a77ab854956750ef6e77da675b06448e3a
Siddhartha Sankar ghosh <ghoshsiddhartha456@gmail.com>
2024-12-03 08:53:20 +0530

    First commit
    Date:03-12-20244

---
+++ build/precache-manifest.51e5022150abea23d33ca1361fdfad08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748b3fa3987f4e7c56336c59e494df8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f36876dc8869baf949a",
+    "url": "/static/css/main.7e5da232.chunk.css"
+  },
+  {
+    "revision": "c3a7c60fa8717c309584",
+    "url": "/static/js/2.72e39330.chunk.js"
+  },
+  {
+    "revision": "67ae3e0212c3a537d86a1f099983ebeb",
+    "url": "/static/js/2.72e39330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a305264669ab0698eb9",
+    "url": "/static/js/3.a8bd2e92.chunk.js"
+  },
+  {
+    "revision": "4f36876dc8869baf949a",
+    "url": "/static/js/main.56e1abf6.chunk.js"
+  },
+  {
+    "revision": "9ce659da1e767b4050ed",
+    "url": "/static/js/runtime-main.d30e4eab.js"
+  }
+]);
